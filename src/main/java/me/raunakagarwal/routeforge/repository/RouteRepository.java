c690621f71656ce77d97072f100c7c8bfc7b2aef
Raunakaga2004 <raunakaga12@gmail.com>
2026-04-05 10:41:18 +0530

    added two repository of route and source

---
+++ src/main/java/me/raunakagarwal/routeforge/repository/RouteRepository.java
@@ -0,0 +1,13 @@
+package me.raunakagarwal.routeforge.repository;
+
+import me.raunakagarwal.routeforge.entity.Route;
+import me.raunakagarwal.routeforge.entity.Source;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+    List<Route> findBySourceDomainIgnoreCase(String domain);
+
+    boolean existsBySourceAndPath(Source source, String path);
+}
